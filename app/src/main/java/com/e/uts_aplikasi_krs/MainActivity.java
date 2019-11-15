d02cb3c953888362f18811d13587f304e97c3ea7
Cindy Claudya <cindyclaudya909@gmail.com>
2019-11-15 23:17:39 +0700

    Login Preferances

--- app/src/main/java/com/e/uts_aplikasi_krs/MainActivity.java
+++ app/src/main/java/com/e/uts_aplikasi_krs/MainActivity.java
@@ -2,7 +2,11 @@ package com.e.uts_aplikasi_krs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import java.util.prefs.Preferences;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-    }
-}
+        findViewById(R.id.btnLogout).setOnClickListener(new View.OnClickListener()
+        {
+            @Override public void onClick(View view) {
+                Preferences.clearLoggedInUser(getBaseContext());
+                startActivity(new
+                        Intent(getBaseContext(),Login.class));
+                finish();
+            }
+
+    });
+
+}}
